066cbbd4fa3e170233288cb502973c70b59886c9
Imral Okyar <100049698+Aymril@users.noreply.github.com>
2022-04-21 01:24:12 +0300

    Create iterative_sol.cpp

---
+++ SIngly Linked Lists/Remove Duplicates/iterative_sol.cpp
@@ -0,0 +1,17 @@
+Node* remove_duplicates(Node* list) {
+    if (!list) return list;
+
+    Node* temp, * head = list;
+    while (list->link) {
+        if (list->data == list->link->data) {
+            temp = list->link;
+            list->link = temp->link;
+            delete temp;
+        }
+        else {
+            list = list->link;
+        }
+    }
+
+    return head;
+}
