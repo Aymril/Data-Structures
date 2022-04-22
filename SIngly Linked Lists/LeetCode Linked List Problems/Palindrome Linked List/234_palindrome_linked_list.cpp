7d30b624542bf8d78aaf8485241470a18973bece
Imral Okyar <100049698+Aymril@users.noreply.github.com>
2022-04-23 02:02:09 +0300

    Update 234_palindrome_linked_list.cpp

--- SIngly Linked Lists/LeetCode Linked List Problems/Palindrome Linked List/234_palindrome_linked_list.cpp
+++ SIngly Linked Lists/LeetCode Linked List Problems/Palindrome Linked List/234_palindrome_linked_list.cpp
@@ -1,1 +1,35 @@
-fsa
+
+   
+Node* middleNode(Node* head) {
+    Node* slow = head, * fast = head;
+
+    while (fast && fast->link) {
+        slow = slow->link;
+        fast = fast->link->link;
+    }
+
+    return slow;
+}
+
+void isPalindromeTest(Node** head, Node* middle, bool* isTrue) {
+    
+
+    if (middle->link)
+        isPalindromeTest(head, middle->link, isTrue);
+
+    if (*isTrue == false)
+        return;
+
+    if ((*head)->data != middle->data)
+        *isTrue = false;     
+    else {
+        (*head) = (*head)->link;
+        *isTrue = true;
+    }
+}
+
+bool isPalindrome(Node* head) {
+    bool result = true;
+    isPalindromeTest(&head, middleNode(head), &result);
+    return result;
+}
