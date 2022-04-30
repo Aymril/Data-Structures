7f7bdc6b22e66069766ceef9d4ccb57b9f76e1ce
Imral Okyar <100049698+Aymril@users.noreply.github.com>
2022-04-30 13:12:59 +0300

    Update local_reference_solution.cpp

--- SIngly Linked Lists/Merge 2 Sorted lists/local_reference_solution.cpp
+++ SIngly Linked Lists/Merge 2 Sorted lists/local_reference_solution.cpp
@@ -1,4 +1,7 @@
-/* linus torvalds tedX konusmasinda "good taste" meselesini anlatmak icin linked list uzerinden bir ornek verdi. ozetle dedigi su sekildeydi: insertion/deletion vb benzer islemler icin corner case (kose durumlari) olabilir. bu gibi durumlarda isleme tabi tutulacak node'un evvelindeki node'un adresini tutmamiz gerekiyor. local reference kullanarak corner case'ler icin ayri ayri kod yazmaktan kurtulabiliriz. her case'i kapsayan bir kod yaziliyor local reference ile: ornegin, */
+/* linus torvalds tedX konusmasinda "good taste" meselesini anlatmak icin linked list uzerinden bir ornek verdi. 
+ozetle dedigi su sekildeydi: insertion/deletion vb benzer islemler icin corner case (kose durumlari) olabilir. bu gibi durumlarda isleme tabi tutulacak node'un 
+evvelindeki node'un adresini tutmamiz gerekiyor. local reference kullanarak corner case'ler icin ayri ayri kod yazmaktan kurtulabiliriz. 
+her case'i kapsayan bir kod yaziliyor local reference ile: ornegin, */
 
 Node* mergeLists(Node* h1, Node* h2) {
     Node** result = &h1, *temp;
