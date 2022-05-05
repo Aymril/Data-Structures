f5baa25b1a94dbd258db03b14603884c423d07e9
Imral Okyar <100049698+Aymril@users.noreply.github.com>
2022-05-06 00:28:02 +0300

    Update 160_intersection_of_two_linked_lists.cpp

--- SIngly Linked Lists/LeetCode Linked List Problems/160_intersection_of_two_linked_lists.cpp
+++ SIngly Linked Lists/LeetCode Linked List Problems/160_intersection_of_two_linked_lists.cpp
@@ -20,3 +20,66 @@ Node* getIntersectionNode(Node* headA, Node* headB) {
     }
     return a_ptr;
 }
+
+
+// Cozum 2: Floyd's Cycle Detection algorithm used to find head node of the cycle, if any.
+
+Node* cycleHeadNode(Node* head) {
+    Node* slow = head, * fast = head;
+    while (slow && fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            while (head != slow) {
+                slow = slow->next;
+                head = head->next;
+            }
+            return head;
+        }
+    }
+    return nullptr;
+}
+
+Node* getIntersectionNodeFloyd(Node* headA, Node* headB) {
+    Node* temp = headA;
+    while (headA->next) {
+        headA = headA->next;
+    }
+    headA->next = headB;
+    Node* remember = headA;
+
+    Node* intersectionNode = cycleHeadNode(temp);
+    remember->next = nullptr;
+    headA = temp;
+    return intersectionNode;
+}
+
+// Cozum 3: Cozum 2'nin degistirilmis/farkli dusunulmus versiyonu
+
+Node* cycleHeadNode(Node* head) {
+    Node* slow = head, * fast = head;
+    while (slow && fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            while (head != slow) {
+                slow = slow->next;
+                head = head->next;
+            }
+            return head;
+        }
+    }
+    return nullptr;
+}
+
+Node* getIntersectionNodeFloydCircular(Node* headA, Node* headB) {
+    Node* originalHeadA = headA;
+    while (headA->next) {
+        headA = headA->next;
+    }
+    headA->next = originalHeadA;
+
+    Node* hasCycle = cycleHeadNode(headB);
+    headA->next = nullptr;
+    return hasCycle;
+}
